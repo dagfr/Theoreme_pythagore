cf2659baa5c6673415ec8d0e548065b3b81726be
dagfr <dagfr@users.noreply.github.com>
2020-07-27 12:13:47 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,44 +2,51 @@ import math
 import sys
 
 units = ["km", "hm", "dam", "m", "dm", "cm", "mm"]
-unit = input("entrer l'unité des longueur du triangle")
+unit = input("entrez l'unité de longueur du triangle" : ) 
 
-if not unit in units:
-    print("veuillez n'entrer que une unité de longueur valide")
+if unit not in units:
+    print("veuillez n'entrer une des unités de longueur valides : {} ".format(units)) 
     sys.exit()
 
-name_triangle = input("entrer le nom du triangle")  # nom du triangle
+name_triangle = input("entrez le nom du triangle (ex. ABC) : ").upper()  
 
-right_angle = input("entrer le sommet de l'angle droit")  # sommet de l'angle droit
+if len(name_triangle) != 3 or not name_triangle.isalpha():
+    print("veuillez entrer un nom valide (ex. ABC)") 
+    sys.exit()
 
-hypothenuse = input("entrer la nom du coté de l'hypothenuse")
+right_angle = input("entrer le sommet de l'angle droit (ex. A): ").upper()  
 
-side_triangle3 = input("entrer la nom du coté du 3eme coté")
-side_triangle2 = input("entrer la nom du coté du 2eme coté")
+if len(right_angle) != 1 or right_angle not in name_triangle:
+    print("veuillez entrez un sommet valide du triangle {} (ex. {}) ".format(name_triangle, name_triangle[0])) #typo and message
+    sys.exit()
 
-length_side2 = input("entrer la longueur du 2eme coté")
-length_side3 = input("entrer la longueur du 3eme coté")
+hypothenuse = "".join([a for a in name_triangle if a != right_angle]) 
+side_triangle2,side_triangle3 = right_angle+hypothenuse[0], hypothenuse[1]+right_angle
+
+length_side2 = input("entrer la longueur du coté {} en {}".format(side_triangle2,unit)) 
+length_side3 = input("entrer la longueur du coté {} en {}".format(side_triangle3,unit))
 
 if not length_side2.isdigit() or not length_side3.isdigit():
     print("Vous ne devez entrer que des nombres, pas de lettre ni de caractere speciaux")
     sys.exit()
 
 
-print(f"le triangle {name_triangle} est rectangle en {right_angle} d'hypothenuse {hypothenuse}")
-print("déprès le théoreme de phytagore:")
-print(f"on a: {hypothenuse}² = {side_triangle2}² + {side_triangle3}²")
-print(f"      {hypothenuse}² = {length_side2}² + {length_side3}²")
+print("le triangle {} est rectangle en {} d'hypothenuse {}".format(name_triangle,right_angle,hypothenuse)
+print("d'après le théoreme de phytagore:")
+print("on a: {}² = {}² + {}²".format(hypothenuse,side_triangle2,side_triangle3))
+print("      {}² = {}² + {}²".format(hypothenuse,length_side2,length_side3))
 
 side2_length_squared = int(length_side2) ** 2
 side3_length_squared = int(length_side3) ** 2
+hypo = math.sqrt(side2_length_squared + side3_length_squared)      
 
 print(f"      {hypothenuse}² = {side2_length_squared} + {side2_length_squared}")
 print(f"      {hypothenuse}² = {side2_length_squared + side3_length_squared}")
-print(f"      {hypothenuse}² = √{side2_length_squared + side3_length_squared}")
-print(f"      {hypothenuse} = {math.sqrt(side2_length_squared + side3_length_squared)}")
-print(f"      {hypothenuse} = {math.floor(math.sqrt(side2_length_squared + side3_length_squared))}")
+print(f"      {hypothenuse} = √{side2_length_squared + side3_length_squared}")
+print(f"      {hypothenuse} = {hypo}")
+print(f"      {hypothenuse} = {math.floor(hypo)}")
 
-print(f"      L'ypothénuse mesure {math.floor(math.sqrt(side2_length_squared + side3_length_squared))} {unit}")
+print(f"      L'hypothénuse {hypothenuse} mesure {math.floor(hypo)} {unit}")
 
 
 
